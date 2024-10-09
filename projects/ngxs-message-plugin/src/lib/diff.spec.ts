46520235edfd73d46873e18c570899b23407f322
Andreas Hjortland <andhjort@acos.no>
2024-10-09 13:47:01 +0200

    Fix issue with array inheritance, dates and regexes

--- projects/ngxs-message-plugin/src/lib/diff.spec.ts
+++ projects/ngxs-message-plugin/src/lib/diff.spec.ts
@@ -10,7 +10,7 @@ type State = {
     foo: string;
 }
 
-describe('HostFeatures', () => {
+describe('DiffAlgorithm', () => {
     let lastState: State;
     let nextState: State;
     let diff: Diff[];
@@ -51,5 +51,119 @@ describe('HostFeatures', () => {
     it('should keep reference to untouched parts of state', () => {
         const result = applyDiff(lastState, diff);
         expect(result.test.baz).toBe(lastState.test.baz);
+    });
+
+    it('should keep prototype chain of classes', () => {
+        class Foo {
+            bar: string;
+            constructor(bar: string) {
+                this.bar = bar;
+            }
+
+            getBar() {
+                return this.bar;
+            }
+        }
+        class Bar extends Foo {
+            baz: string;
+            constructor(bar: string, baz: string) {
+                super(bar);
+                this.baz = baz;
+            }
+
+            override toString() {
+                return `${this.getBar()}:${this.baz}`;
+            }
+        }
+
+        const lastState = {
+            test: new Bar('bar', 'baz'),
+            foo: 'asdf',
+        };
+        const diff: Diff[] = [
+            { key: 'test.bar', operation: 'update', value: 'qux' },
+        ];
+        const result = applyDiff(lastState, diff);
+        expect(result.test).toBeInstanceOf(Bar);
+        expect(result.test.toString()).toEqual('qux:baz');
+    });
+
+    it('should keep prototype chain of arrays', () => {
+        class Foo extends Array<string> {
+            bar: string;
+            constructor(bar: string) {
+                super();
+                this.bar = bar;
+            }
+
+            doSomething() {
+                return `${this.bar}: ${this.join(',')}`;
+            }
+        }
+
+        const lastState = new Foo('bar');
+        lastState.push('baz', 'qux', 'quux');
+        const diff: Diff[] = [
+            { key: '1', operation: 'update', value: 'changed' },
+            { key: 'bar', operation: 'update', value: 'test' },
+        ];
+        const result = applyDiff(lastState, diff);
+        expect(result).toBeInstanceOf(Foo);
+        expect(result.doSomething()).toEqual('test: baz,changed,quux');
+        expect(result[1]).toEqual('changed');
+        expect(result.bar).toEqual('test');
+    });
+
+    it('should throw on invalid diff', () => {
+        const diff: Diff[] = [
+            { key: 'test.foo.bar', operation: 'update', value: 'value' },
+        ];
+        expect(() => applyDiff(lastState, diff)).toThrow();
+    });
+
+    it('should handle different instance types', () => {
+        class Foo {
+            text: string;
+            constructor(text: string) {
+                this.text = text;
+            }
+        }
+        class Bar {
+            text: string;
+            bar = 'content';
+            constructor(text: string) {
+                this.text = text;
+            }
+        }
+        const lastState = {
+            instance: new Foo('test'),
+        };
+        const nextState = {
+            instance: new Bar('test'),
+        };
+        const diff = Array.from(getDiff(lastState, nextState));
+        const result = applyDiff(lastState, diff);
+        expect(result).toEqual(nextState);
+    });
+
+    // TODO: Add special handling for builtin objects
+    it('should be able to clone internal objects', () => {
+        const prev = {
+            foo: 'bar',
+            date: new Date('2020-01-01'),
+            regex: /test/,
+            //map: new Map([['test', 'removed']]),
+            //set: new Set([1, 2, 3]),
+        };
+        const next = {
+            foo: 'bar',
+            date: new Date('2024-01-01'),
+            regex: /test/i,
+            //map: new Map<string | number, string | number>([[1, 2], ['foo', 'bar']]),
+            //set: new Set([2, 3, 4]),
+        };
+        const diff = Array.from(getDiff(prev, next));
+        console.log('diff', Array.from(diff));
+        expect(applyDiff(prev, diff)).toEqual(next);
     });
   });
--- projects/ngxs-message-plugin/src/lib/diff.ts
+++ projects/ngxs-message-plugin/src/lib/diff.ts
@@ -1,62 +1,115 @@
+/**
+ * Diff between two objects
+ */
 export type Diff = {
     key: string; // key of the object, dots for nesting
     operation: 'add' | 'remove' | 'update';
     value?: any;
 };
 
-export function* getDiff<T>(oldState: T, nextState: T, visited: WeakSet<any> = new WeakSet(), existingKey: string = ''): Iterable<Diff> {
-    if (typeof oldState === 'object' && oldState) {
-        if (visited.has(oldState)) {
+/**
+ * Shallow clone an object, keeping prototype chain of classes
+ *
+ * @param obj Object to clone
+ * @returns Shallow clone of input object
+ */
+export function clone<T>(obj: T): T {
+    let newVal: T;
+    if(Array.isArray(obj)) {
+        newVal = [...obj] as T;
+        const proto = Object.getPrototypeOf(obj);
+        if(proto !== Array.prototype) {
+            Object.setPrototypeOf(newVal, proto);
+            for(const key in obj) {
+                if(!/^\d+$/.test(key) && Object.hasOwn(obj, key)) {
+                    (newVal as any)[key] = obj[key];
+                }
+            }
+        }
+    } else {
+        newVal = Object.create(Object.getPrototypeOf(obj));
+        Object.assign(newVal as {}, obj);
+    }
+    return newVal;
+}
+
+function* _getDiff<T>(prev: T, current: T, visited: WeakSet<any>, existingKey: string): Iterable<Diff> {
+    if (typeof prev === 'object' && prev) {
+        if (visited.has(prev)) {
             return;
         }
-        visited.add(oldState);
+        visited.add(prev);
     }
 
 
-    if (typeof oldState !== 'object' || oldState === null || typeof nextState !== 'object' || nextState === null) {
-        if (oldState !== nextState) {
-            yield { key: existingKey, operation: 'update', value: nextState };
+    if (typeof prev !== 'object' || prev === null || typeof current !== 'object' || current === null) {
+        if (prev !== current) {
+            yield { key: existingKey, operation: 'update', value: current };
         }
     } else {
-        for (let propKey in oldState) {
+        for (let propKey in prev) {
             const key = existingKey + propKey;
-            if (oldState[propKey] === nextState[propKey]) {
+            if (prev[propKey] === current[propKey]) {
                 continue;
             }
 
-            if (!(propKey in nextState) || typeof nextState[propKey] === 'undefined' || nextState[propKey] === null) {
+            if (!(propKey in current) || typeof current[propKey] === 'undefined' || current[propKey] === null) {
                 yield { key, operation: 'remove' };
-            } else if (typeof oldState[propKey] !== typeof nextState[propKey]) {
-                yield { key, operation: 'update', value: nextState[propKey] };
-            } else if (typeof oldState[propKey] !== 'object') {
-                yield { key, operation: 'update', value: nextState[propKey] };
+            } else if (typeof prev[propKey] !== typeof current[propKey]) {
+                yield { key, operation: 'update', value: current[propKey] };
+            } else if (typeof prev[propKey] !== 'object' || typeof(current[propKey]) !== 'object' || Object.getPrototypeOf(prev[propKey]) !== Object.getPrototypeOf(current[propKey])) {
+                yield { key, operation: 'update', value: current[propKey] };
+            } else if(prev[propKey] instanceof Date) {
+                if((prev[propKey] as Date).getTime() !== (current[propKey] as unknown as Date).getTime()) {
+                    yield { key, operation: 'update', value: current[propKey] };
+                }
+            } else if(prev[propKey] instanceof RegExp) {
+                if((prev[propKey] as RegExp).toString() !== (current[propKey] as unknown as RegExp).toString()) {
+                    yield { key, operation: 'update', value: current[propKey] };
+                }
             } else {
-                // TODO: dates?
-                // TODO: regexes?
-                // TODO: Iterables?
-                yield* getDiff(oldState[propKey], nextState[propKey], visited, `${existingKey}${propKey}.`);
+                // TODO: Map, Set, Iterables?
+                yield* _getDiff(prev[propKey], current[propKey], visited, `${existingKey}${propKey}.`);
             }
         }
-        for (let propKey in nextState) {
+        for (let propKey in current) {
             const key = existingKey + propKey;
-            if (!(propKey in oldState) && typeof nextState[propKey] !== 'undefined' && nextState[propKey] !== null) {
-                yield { key, operation: 'add', value: nextState[propKey] };
+            if (!(propKey in prev) && typeof current[propKey] !== 'undefined' && current[propKey] !== null) {
+                yield { key, operation: 'add', value: current[propKey] };
             }
         }
     }
 }
 
-export function applyDiff<T>(oldState: T, diffs: Diff[]): T {
-    let nextState = { ...oldState };
+/**
+ * Create a diff between two states
+ * @param prev Previous version of the state
+ * @param current Next version of the state
+ * @returns A list of diffs between the two states
+ */
+export function getDiff<T>(prev: T, current: T): Iterable<Diff> {
+    return _getDiff(prev, current, new WeakSet(), '');
+}
+
+/**
+ * Apply diffs to a state
+ *
+ * @param oldState State to update
+ * @param diffs List of diffs to apply
+ * @returns A new instance of the state with the diffs applied. Any object that is not touched by the diff will be the same reference as in the old state.
+ * @throws If a diff key does not exist in the state
+ */
+export function applyDiff<T>(oldState: T, diffs: Iterable<Diff>): T {
+    let nextState = clone(oldState);
     for (let diff of diffs) {
         const parts = diff.key.split('.');
         let val: any = nextState;
         for(let part of parts.slice(0, -1)) {
-            if (Array.isArray(val[part])) {
-                val = val[part] = [...val[part]];
-            } else {
-                val = val[part] = { ...val[part] };
+            if(typeof val[part] !== 'object' || val[part] === null) {
+                console.error('Invalid diff key', diff.key, 'does not exist in state', oldState, 'Navigated to', val);
+                throw new Error(`Invalid diff key ${diff.key} does not exist in state`);
             }
+            val = val[part] = clone(val[part]);
         }
         let lastPart = parts.at(-1)!;
         switch (diff.operation) {
